0cb0186d1a8077edba7b1ab97542e829395daa95
yongjunchai <yongjunchai@hotmail.com>
2019-09-21 22:19:29 -0700

    double knapsack solution and unit test

--- algorithmIlliminated_3/dpEx.py
+++ algorithmIlliminated_3/dpEx.py
@@ -1,3 +1,4 @@
+
 class Item:
     def __init__(self, index: int, value: int, size: int):
         assert (type(size) is int and size > 0)
@@ -19,6 +20,39 @@ class DpEx:
     def __init__(self):
         pass
 
+    def constructDoubleKnapsackSolution(self, c1, c2, items, subProblemSolutions):
+        assert(type(c1) is int)
+        assert(type(c2) is int)
+        assert(type(items) is list)
+        assert(type(subProblemSolutions) is list)
+        ascii(len(subProblemSolutions) == len(items) + 1)
+        ascii(len(subProblemSolutions[0]) == c2 + 1)
+        ascii(len(subProblemSolutions[0][0]) == c1 + 1)
+        solution = list()
+        i = len(items)
+        j = c2
+        k = c1
+        while i > 0:
+            if subProblemSolutions[i][j][k] <= subProblemSolutions[i - 1][j][k]:
+                i = i - 1
+                continue
+            c1Val = -1
+            c2Val = -1
+            if items[i - 1].size <= k:
+                c1Val = subProblemSolutions[i - 1][j][k - items[i - 1].size] + items[i - 1].value
+            if items[i - 1].size <= j:
+                c2Val = subProblemSolutions[i - 1][j - items[i - 1].size][k] + items[i - 1].value
+            if c1Val >= subProblemSolutions[i][j][k]:
+                k = k - items[i - 1].size
+                solution.append(("c1", items[i - 1]))
+            elif c2Val >= subProblemSolutions[i][j][k]:
+                j = j - items[i - 1].size
+                solution.append(("c2", items[i - 1]))
+            else:
+                assert(False)
+            i = i - 1
+        return solution
+
     def solveDoubleKnapsack(self, c1, c2, items: list):
         assert(type(c1) is int)
         assert(type(c2) is int)
@@ -29,9 +63,24 @@ class DpEx:
                 subProblemSolutions[0][i][j] = 0
         for i in range(len(items) + 1):
             subProblemSolutions[i][0][0] = 0
-        for i in range(len(items) + 1):
+        for i in range(1, len(items) + 1):
             for j in range(c2 + 1):
                 for k in range(c1 + 1):
-                    c1Val = 0
+                    c1Val = -1
+                    c2Val = -1
+                    if items[i - 1].size <= k:
+                        c1Val = subProblemSolutions[i - 1][j][k - items[i - 1].size] + items[i - 1].value
+                    if items[i - 1].size <= j:
+                        c2Val = subProblemSolutions[i - 1][j - items[i - 1].size][k] + items[i - 1].value
+                    c3Val = subProblemSolutions[i - 1][j][k]
+                    if c2Val <= c1Val and c3Val <= c1Val:
+                        subProblemSolutions[i][j][k] = c1Val
+                    elif c3Val <= c2Val and c1Val <= c2Val:
+                        subProblemSolutions[i][j][k] = c2Val
+                    else:
+                        subProblemSolutions[i][j][k] = c3Val
+        return subProblemSolutions
+
+
 
 
--- algorithmIlliminated_3/testDpEx.py
+++ algorithmIlliminated_3/testDpEx.py
@@ -4,12 +4,33 @@ from algorithmIlliminated_3.dpEx import *
 
 class DpExTestCase(unittest.TestCase):
     def test_solveDoubleKnapsack(self):
-        items = [Item(1, 3, 4), Item(2, 2, 3), Item(3, 4, 2), Item(4, 4, 3)]
-        c1 = 2
-        c2 = 3
-        dpEx = DpEx()
-        dpEx.solveDoubleKnapsack(c1, c2, items)
+        dataSet = [(([Item(1, 2, 3), Item(2, 3, 4), Item(3, 4, 2), Item(4, 4, 3)], 5, 6), ({"c1": [3, 4], "c2": [2]})),
+                   (([Item(1, 2, 3), Item(2, 3, 4), Item(3, 4, 4), Item(4, 4, 3)], 2, 100), ({"c1": [], "c2": [1, 2, 3, 4]})),
+                   (([Item(1, 2, 7), Item(2, 3, 18), Item(3, 4, 100), Item(4, 4, 101)], 25, 201), ({"c1": [1, 2], "c2": [3, 4]})),
+                    (([Item(1, 2, 7), Item(2, 3, 18), Item(3, 4, 100), Item(4, 4, 101), Item(5, 4, 30)], 201, 55), ({"c1": [3, 4], "c2": [1, 2, 5]})),
+                   (([Item(1, 2, 7), Item(2, 3, 18), Item(3, 4, 100)], 100, 25), ({"c1": [3], "c2": [1, 2]})),
 
+                   ]
+        for data in dataSet:
+            expected = data[1]
+            items = data[0][0]
+            c1 = data[0][1]
+            c2 = data[0][2]
+            dpEx = DpEx()
+            subProblemSolutions = dpEx.solveDoubleKnapsack(c1, c2, items)
+            solution = dpEx.constructDoubleKnapsackSolution(c1, c2, items, subProblemSolutions)
+            results = dict()
+            for i in solution:
+                bagName = i[0]
+                item = i[1]
+                if results.get(bagName) is None:
+                    results[bagName] = list()
+                results[bagName].append(item.index)
+            for bagName in results:
+                results[bagName] = sorted(results[bagName])
+            print(results)
+            for bagName in results:
+                assert(expected[bagName] == results[bagName])
 
 if __name__ == '__main__':
     unittest.main()
