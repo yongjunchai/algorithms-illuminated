b5e576bdcfaeba431d86706dd76e76300b29dfcd
yongjunchai <yongjunchai@hotmail.com>
2019-09-22 18:39:53 -0700

    solve multiple knapsacks problem

--- algorithmIlliminated_3/dpEx.py
+++ algorithmIlliminated_3/dpEx.py
@@ -1,4 +1,4 @@
-
+from algorithmIlliminated_3.utils import *
 class Item:
     def __init__(self, index: int, value: int, size: int):
         assert (type(size) is int and size > 0)
@@ -57,7 +57,7 @@ class DpEx:
         assert(type(c1) is int)
         assert(type(c2) is int)
         assert(type(items) is list)
-        subProblemSolutions = [[[None for i in range(c1 + 1)] for j in range(c2 + 1)] for k in range(len(items) + 1)]
+        subProblemSolutions = Utils.createArray([len(items) + 1, c2 + 1, c1 + 1])
         for i in range(c2 + 1):
             for j in range(c1 + 1):
                 subProblemSolutions[0][i][j] = 0
@@ -80,6 +80,84 @@ class DpEx:
                     else:
                         subProblemSolutions[i][j][k] = c3Val
         return subProblemSolutions
+
+    def  pickKnapsack(self, subProblemSolutions, indices: list, items: list, dimensions: int):
+        result = [-1 for i in range(dimensions)]
+        for i in range(1, dimensions):
+            if indices[i] < items[indices[0] - 1].size:
+                continue
+            copiedIndices = indices.copy()
+            copiedIndices[i] = copiedIndices[i] - items[indices[0] - 1].size
+            value = Utils.getValue(subProblemSolutions, copiedIndices) + items[indices[0] - 1].value
+            result[i] = value
+        pickedValue = -1
+        pickedIndex = -1
+        for i in range(len(result)):
+            if result[i] > pickedValue:
+                pickedValue = result[i]
+                pickedIndex = i
+        return pickedIndex
+
+    def solveKnapsacks(self, subProblemSolutions, indices: list, items: list, dimensions: int):
+        if len(indices) < dimensions:
+            slot = Utils.getValue(subProblemSolutions, indices)
+            for i in range(len(slot)):
+                self.solveKnapsacks(subProblemSolutions, indices.copy().append(i), items, dimensions)
+        else:
+            i = self.pickKnapsack(subProblemSolutions, indices)
+            if i < 1:
+                copiedIndices = indices.copy()
+                copiedIndices[0] = copiedIndices[0] - 1
+                Utils.updateValue(subProblemSolutions, indices, Utils.getValue(subProblemSolutions, copiedIndices))
+            else:
+                copiedIndices = indices.copy()
+                copiedIndices[0] = copiedIndices[0] - 1
+                preVal = Utils.getValue(subProblemSolutions, copiedIndices)
+
+                copiedIndices = indices.copy()
+                copiedIndices[i] = copiedIndices[i] - items[indices[0] - 1].size
+                value = Utils.getValue(subProblemSolutions, copiedIndices) + items[indices[0] - 1].value
+                if preVal > value:
+                    Utils.updateValue(subProblemSolutions, indices, preVal)
+                else:
+                    Utils.updateValue(subProblemSolutions, indices, value)
+
+    def solveKnapsacks(self, knapsacks: list, items: list):
+        """
+        :param knapsacks: a list of knapsacks. The value of each knapsack is the capacity of the knapsack.
+        :param items: the items to be distributed into the knapsacks. each item has files: index, value, size
+        :return: the solution to sub problems
+        """
+        for i in knapsacks:
+            knapsacks[i] = knapsacks[i] + 1
+        dimensions = [len(items) + 1]
+        dimensions.extend(knapsacks)
+        subProblemSolutions = Utils.createArray(dimensions)
+        Utils.updateValue(subProblemSolutions, [0], 0)
+        for i in range(1, len(items) + 1):
+            self.solveKnapsacks(subProblemSolutions, [i], items, len(knapsacks) + 1)
+        return subProblemSolutions
+
+    def constructKnapsacksSolution(self, subProblemSolutions, knapsacks: list, items: list):
+        indices = [len(items)]
+        indices.extend(knapsacks)
+        i = len(items)
+        solution = dict()
+        while i > 0:
+            indices[0] = i
+            copiedIndices = indices.copy()
+            copiedIndices[0] = copiedIndices[0] - 1
+            if Utils.getValue(subProblemSolutions, indices) <= Utils.getValue(copiedIndices):
+                i = i - 1
+                continue
+            j = self.pickKnapsack(subProblemSolutions, indices, items, len(indices))
+            assert(j > 0)
+            if solution.get(str(j)) is None:
+                solution.update(str(j), [])
+            solution.get(str(j)).append(items[i - 1].index)
+            indices[j] = indices[j] - items[i - 1].size
+            i = i - 1
+        return solution
 
 
 
