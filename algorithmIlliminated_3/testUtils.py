a699250b999f1070ba97c054b61f9e1f055aad51
yongjunchai <yongjunchai@hotmail.com>
2019-10-05 23:40:10 -0700

    add unit test to high dimension array manipulation

---
+++ algorithmIlliminated_3/testUtils.py
@@ -0,0 +1,21 @@
+import unittest
+from algorithmIlliminated_3.utils import *
+
+
+class UtilsTestCase(unittest.TestCase):
+    def test_multiDimensionArray(self):
+        array = Utils.createArray([3, 4, 5])
+        self.assertTrue(len(array) == 3)
+        self.assertTrue(len(array[0]) == 4)
+        self.assertTrue(len(array[0][0]) == 5)
+
+    def test_valueUpate(self):
+        array = Utils.createArray([2, 2, 2, 2, 2])
+        Utils.updateValue(array, [1], 2)
+        print(array)
+        print(Utils.getValue(array, [1, 1, 1, 1, 1]))
+
+
+
+if __name__ == '__main__':
+    unittest.main()
