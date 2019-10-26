7d9ee7d9fe4d611e026e773a2c6634d04bc3710f
yongjunchai <yongjunchai@hotmail.com>
2019-10-26 09:41:01 -0700

    add unit test

--- algorithmIlliminated_3/optBst.py
+++ algorithmIlliminated_3/optBst.py
@@ -25,10 +25,10 @@ class OptBst:
             assert(i == nodes[i - 1].key)
             assert (nodes[i - 1].frequency > 0)
         # subproblems (i indexed from 1, j indexed from 0)
-        subProblems = Utils.createArray([len(nodes) + 2, len(nodes) + 1])
+        subProblemsSolution = Utils.createArray([len(nodes) + 2, len(nodes) + 1])
         # base case
         for i in range(1, len(nodes) + 2):
-            subProblems[i][i - 1] = 0
+            subProblemsSolution[i][i - 1] = 0
         for size in range(1, len(nodes) + 1):
             for i in range(1, len(nodes) + 1 - size + 1):
                 sumFrequency = 0
@@ -36,11 +36,11 @@ class OptBst:
                     sumFrequency = sumFrequency + nodes[k - 1].frequency
                 minSubs = sys.maxsize
                 for r in range(i, i + size):
-                    value = subProblems[i][r - 1] + subProblems[r + 1][i + size - 1]
+                    value = subProblemsSolution[i][r - 1] + subProblemsSolution[r + 1][i + size - 1]
                     if value < minSubs:
                         minSubs = value
-                subProblems[i][i + size - 1] = sumFrequency + minSubs
-        return subProblems
+                subProblemsSolution[i][i + size - 1] = sumFrequency + minSubs
+        return subProblemsSolution
 
 
     def solveOptBst_n2(self, nodes: list):
@@ -57,44 +57,44 @@ class OptBst:
             for size in range(2, len(nodes) - i + 1):
                 frequencySums[i][i + size - 1] = frequencySums[i][i + size - 2] + nodes[i + size - 1].frequency
 
-        # prepare for subProblemsSum
-        subProblemsSum = Utils.createArray([len(nodes) + 2, len(nodes) + 1])
+        # prepare for subProblemsSolution
+        subProblemsSolution = Utils.createArray([len(nodes) + 2, len(nodes) + 1])
         # base case, size == 1
         for i in range(1, len(nodes) + 1):
-            subProblemsSum[i][i] = RootNode(nodes[i - 1].key, nodes[i - 1].frequency)
+            subProblemsSolution[i][i] = RootNode(nodes[i - 1].key, nodes[i - 1].frequency)
         for i in range(1, len(nodes) + 2):
-            subProblemsSum[i][i - 1] = RootNode(0, 0)
+            subProblemsSolution[i][i - 1] = RootNode(0, 0)
         for size in range(2, len(nodes) + 1):
             for i in range(1, len(nodes) - size + 1 + 1):
-                minRoot = subProblemsSum[i][i + size - 1 - 1]
-                maxRoot = subProblemsSum[i + 1][i + size - 1]
+                minRoot = subProblemsSolution[i][i + size - 1 - 1]
+                maxRoot = subProblemsSolution[i + 1][i + size - 1]
                 minSubRoot = RootNode(0, sys.maxsize)
                 for j in range(minRoot.key, maxRoot.key + 1):
-                    value = subProblemsSum[i][j - 1].totalSum + subProblemsSum[j + 1][i + size - 1].totalSum
+                    value = subProblemsSolution[i][j - 1].totalSum + subProblemsSolution[j + 1][i + size - 1].totalSum
                     if value < minSubRoot.totalSum:
                         minSubRoot.key = j
                         minSubRoot.totalSum = value
                 minSubRoot.totalSum = minSubRoot.totalSum + frequencySums[i - 1][i + size - 1 - 1]
-                subProblemsSum[i][i + size - 1] = minSubRoot
-        return subProblemsSum
+                subProblemsSolution[i][i + size - 1] = minSubRoot
+        return subProblemsSolution
 
-    def constructOptBST(self, subProblems, nodes: list, left: int, right: int):
+    def constructOptBST(self, subProblemsSolution, nodes: list, left: int, right: int):
         """
-        :param subProblems: subproblems (i indexed from 1 to n + 1, j indexed from 0 to n - 1)
+        :param subProblemsSolution: subproblems (i indexed from 1 to n + 1, j indexed from 0 to n - 1)
         :param nodes:
         :param left: index of nodes
         :param right: index of nodes
         :return: root node of the OptBST tree
         """
-        assert(0 <= left < len(nodes))
+        assert(0 <= left <= len(nodes))
         if left > right:
             return None
         if left == right:
-            return nodes[left]
-        rootNode = subProblems[left + 1][right]
+            return nodes[left - 1]
+        rootNode = subProblemsSolution[left][right]
         node = Node(rootNode.key, nodes[rootNode.key - 1].frequency)
-        node.left = self.constructOptBST(subProblems, nodes, left, rootNode.key - 2)
-        node.right = self.constructOptBST(subProblems, nodes, rootNode.key, right)
+        node.left = self.constructOptBST(subProblemsSolution, nodes, left, rootNode.key - 1)
+        node.right = self.constructOptBST(subProblemsSolution, nodes, rootNode.key + 1, right)
         return node
 
 
--- algorithmIlliminated_3/testOptBst.py
+++ algorithmIlliminated_3/testOptBst.py
@@ -13,14 +13,33 @@ class OptBstTestCase(unittest.TestCase):
         print(subProblems)
 
     def test_solveOptBst_n2(self):
+        dataSet = [([0.8, 0.1, 0.1], (1, 1.3, None, 2))]
         optBst = OptBst()
-        frequencies = [0.8, 0.1, 0.1]
-        nodes = list()
-        for i in range(len(frequencies)):
-            node = Node(i + 1, frequencies[i])
-            nodes.append(node)
-        subProblems = optBst.solveOptBst_n2(nodes)
-        print(subProblems)
+        for data in dataSet:
+            frequencies = data[0]
+            rootKey = data[1][0]
+            totalSum = data[1][1]
+            leftChildKey = data[1][2]
+            rightChildKey = data[1][3]
+            nodes = list()
+            for i in range(len(frequencies)):
+                node = Node(i + 1, frequencies[i])
+                nodes.append(node)
+            subProblems = optBst.solveOptBst_n2(nodes)
+            self.assertTrue(subProblems[1][len(nodes)].key == rootKey)
+            self.assertTrue(subProblems[1][len(nodes)].totalSum == totalSum)
+
+            root = optBst.constructOptBST(subProblems, nodes, 1, len(nodes))
+            print(root)
+            if root.left is None:
+                self.assertTrue(leftChildKey is None)
+            else:
+                self.assertTrue(root.left.key == leftChildKey)
+            if root.right is None:
+                self.assertTrue(rightChildKey is None)
+            else:
+                self.assertTrue(root.right.key == rightChildKey)
+
 
 if __name__ == '__main__':
     unittest.main()
