c1e3fb087362e7a9c1c43840669864d304422e4f
Yongjun Chai <yongjunchai@hotmail.com>
2019-12-25 17:06:42 -0800

    solve problem 9.7

--- algorithmIlliminated_2/dijkstra.py
+++ algorithmIlliminated_2/dijkstra.py
@@ -20,6 +20,22 @@ class Dijkstra:
             self.cnt = self.cnt + 1
             heap.insert(predecessor.dist + length, (targetNode.name + str(self.cnt), targetNode, predecessor))
 
+    def updateHeapMaxEdge(self, graph: Graph, predecessor: Node, heap: Heap):
+        for edgeItem in predecessor.outflowEdges.items():
+            name = edgeItem[0]
+            length = edgeItem[1]
+            targetNode = graph.nodes.get(name)
+            if targetNode is None:
+                print("error %d" % get_linenumber())
+                continue
+            if targetNode.visited:
+                continue
+            self.cnt = self.cnt + 1
+            maxEdge = predecessor.dist
+            if maxEdge < length:
+                maxEdge = length
+            heap.insert(maxEdge, (targetNode.name + str(self.cnt), targetNode, predecessor))
+
     def search(self, graph: Graph, vertexName: str):
         """
         use dijkstra algorithm to find shortest path to source node
@@ -51,4 +67,38 @@ class Dijkstra:
             node.dist = key
             node.predecessor = predecessor
             self.updateHeap(graph, node, heap)
+        return graph
+
+    def searchMaxEdge(self, graph: Graph, vertexName: str):
+        """
+        Problem 9.7
+        use dijkstra algorithm to find path that include the minimum maximum-edge
+        :param graph:
+        :param vertexName: the source vertex name
+        :return: updated Graph with info annotate on the nodes
+        """
+        sourceNode: Node = graph.nodes.get(vertexName)
+        if sourceNode is None:
+            print("there is no source vertex %d" % get_linenumber())
+            return None
+        # initialization
+        for node in graph.nodes.values():
+            node.dist = sys.maxsize
+            node.visited = False
+
+        sourceNode.dist = 0
+        sourceNode.visited = True
+
+        heap = Heap()
+        self.updateHeapMaxEdge(graph, sourceNode, heap)
+        while heap.size() > 0:
+            key, value = heap.extractMin()
+            node: Node = value[1]
+            predecessor = value[2]
+            if node.visited:
+                continue
+            node.visited = True
+            node.dist = key
+            node.predecessor = predecessor
+            self.updateHeapMaxEdge(graph, node, heap)
         return graph
--- algorithmIlliminated_2/dijkstraTest.py
+++ algorithmIlliminated_2/dijkstraTest.py
@@ -73,6 +73,72 @@ class DijkstraTest(unittest.TestCase):
         dataSet = [self.getData1(), self.getData2(), self.getData3()]
         return dataSet
 
+    def getMaxEdgeData1(self):
+        edge1: Edge = Edge("s", "v", 2)
+        edge2: Edge = Edge("s", "w", 4)
+        edge3: Edge = Edge("v", "w", 3)
+        edge4: Edge = Edge("v", "t", 5)
+        edge5: Edge = Edge("w", "t", 3)
+
+        edges = [edge1, edge2, edge3, edge4, edge5]
+        souceVertex = "s"
+        dists = dict()
+        dists["s"] = Path(0, ["s"])
+        dists["v"] = Path(2, ["s", "v"])
+        dists["w"] = Path(3, ["s", "v", "w"])
+        dists["t"] = Path(3, ["s", "v", "w", "t"])
+
+        graph = Graph(edges)
+        return graph, souceVertex, dists
+
+    def getMaxEdgeData2(self):
+        edge1: Edge = Edge("s", "v", 2)
+        edge2: Edge = Edge("s", "w", 4)
+        edge3: Edge = Edge("v", "w", 3)
+        edge4: Edge = Edge("v", "t", 5)
+        edge5: Edge = Edge("w", "t", 3)
+        edge6: Edge = Edge("v", "x", 4)
+        edge7: Edge = Edge("t", "x", 1)
+        edge8: Edge = Edge("s", "x", 4)
+
+        edges = [edge1, edge2, edge3, edge4, edge5, edge6, edge7, edge8]
+        souceVertex = "s"
+        dists = dict()
+        dists["s"] = Path(0, ["s"])
+        dists["v"] = Path(2, ["s", "v"])
+        dists["w"] = Path(3, ["s", "v", "w"])
+        dists["t"] = Path(3, ["s", "v", "w", "t"])
+        dists["x"] = Path(3, ["s", "v", "w", "t", "x"])
+
+        graph = Graph(edges)
+        return graph, souceVertex, dists
+
+    def getMaxEdgeData3(self):
+        edge1: Edge = Edge("s", "v", 2)
+        edge2: Edge = Edge("s", "w", 4)
+        edge3: Edge = Edge("v", "w", 100)
+        edge4: Edge = Edge("v", "t", 100)
+        edge5: Edge = Edge("w", "t", 3)
+        edge6: Edge = Edge("v", "x", 6)
+        edge7: Edge = Edge("t", "x", 1)
+        edge8: Edge = Edge("s", "x", 100)
+
+        edges = [edge1, edge2, edge3, edge4, edge5, edge6, edge7, edge8]
+        souceVertex = "s"
+        dists = dict()
+        dists["s"] = Path(0, ["s"])
+        dists["v"] = Path(2, ["s", "v"])
+        dists["w"] = Path(4, ["s", "w"])
+        dists["t"] = Path(4, ["s", "w", "t"])
+        dists["x"] = Path(4, ["s", "w", "t", "x"])
+
+        graph = Graph(edges)
+        return graph, souceVertex, dists
+
+    def getMaxEdgeTestData(self):
+        dataSet = [self.getMaxEdgeData1(), self.getMaxEdgeData2(), self.getMaxEdgeData3()]
+        return dataSet
+
     def verifyPah(self, path: deque, node: Node):
         vertex = path.pop()
         self.assertTrue(vertex == node.name)
@@ -89,6 +155,22 @@ class DijkstraTest(unittest.TestCase):
             sourceVertex = data[1]
             dists = data[2]
             dijkstra.search(graph, sourceVertex)
+            self.assertTrue(len(dists) == len(graph.nodes))
+            for item in dists.items():
+                vertex = item[0]
+                path = item[1]
+                node: Node = graph.nodes.get(vertex)
+                self.assertTrue(node.dist == path.length)
+                self.verifyPah(path.path, node)
+
+    def test_minimumMaximumEdgeOnPath(self):
+        dijkstra = Dijkstra()
+        dataSet = self.getMaxEdgeTestData()
+        for data in dataSet:
+            graph = data[0]
+            sourceVertex = data[1]
+            dists = data[2]
+            dijkstra.searchMaxEdge(graph, sourceVertex)
             self.assertTrue(len(dists) == len(graph.nodes))
             for item in dists.items():
                 vertex = item[0]
