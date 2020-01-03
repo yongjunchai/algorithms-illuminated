2abfba71ffa3cfc38fb6a662ecd63c09f0eae561
Yongjun Chai <yongjunchai@hotmail.com>
2020-01-02 20:04:29 -0800

    support min/max heap

--- algorithmIlliminated_2/heap.py
+++ algorithmIlliminated_2/heap.py
@@ -1,16 +1,23 @@
 from collections import deque
+from enum import Enum
+
+
+class HeapType(Enum):
+    MIN = 1
+    MAX = 2
 
 
 class Heap:
     """
-    this is an array based min heap
+    this is an array based min/max heap
     left child index: 2i + 1
     right child index: 2i + 2
     """
 
-    def __init__(self):
+    def __init__(self, heapType: HeapType):
         self.items = deque()
         self.size = 0
+        self.heapType = heapType
 
     # TODO: problem 10.6
 
@@ -29,24 +36,41 @@ class Heap:
         self.items[i], self.items[j] = self.items[j], self.items[i]
 
     def bubbleDown(self, i):
-        minimum = i
-        l = 2 * i + 1
-        r = 2 * i + 2
-        if l < self.size and self.items[minimum][0] > self.items[l][0]:
-            minimum = l
-        if r < self.size and self.items[minimum][0] > self.items[r][0]:
-            minimum = r
-        if minimum != i:
-            self.swapItems(i, minimum)
-            self.bubbleDown(minimum)
+        if self.heapType == HeapType.MIN:
+            minimum = i
+            l = 2 * i + 1
+            r = 2 * i + 2
+            if l < self.size and self.items[minimum][0] > self.items[l][0]:
+                minimum = l
+            if r < self.size and self.items[minimum][0] > self.items[r][0]:
+                minimum = r
+            if minimum != i:
+                self.swapItems(i, minimum)
+                self.bubbleDown(minimum)
+        else:
+            maximum = i
+            l = 2 * i + 1
+            r = 2 * i + 2
+            if l < self.size and self.items[maximum][0] < self.items[l][0]:
+                maximum = l
+            if r < self.size and self.items[maximum][0] < self.items[r][0]:
+                maximum = r
+            if maximum != i:
+                self.swapItems(i, maximum)
+                self.bubbleDown(maximum)
 
     def bubbleUp(self, i):
         if i == 0:
             return
         p = int((i - 1) / 2)
-        if self.items[p][0] > self.items[i][0]:
-            self.swapItems(p, i)
-            self.bubbleUp(p)
+        if self.heapType == HeapType.MIN:
+            if self.items[p][0] > self.items[i][0]:
+                self.swapItems(p, i)
+                self.bubbleUp(p)
+        else:
+            if self.items[p][0] < self.items[i][0]:
+                self.swapItems(p, i)
+                self.bubbleUp(p)
 
     def insert(self, key: int, value):
         self.items.append((key, value))
@@ -57,6 +81,21 @@ class Heap:
         """
         :return: item, item[0] is int, item[1] is an object
         """
+        if self.heapType != HeapType.MIN:
+            raise Exception("not supported method")
+        item = self.items[0]
+        self.items[0] = self.items[self.size - 1]
+        self.size = self.size - 1
+        self.items.pop()
+        self.bubbleDown(0)
+        return item
+
+    def extract_max(self):
+        """
+        :return: item, item[0] is int, item[1] is an object
+        """
+        if self.heapType != HeapType.MAX:
+            raise Exception("not supported method")
         item = self.items[0]
         self.items[0] = self.items[self.size - 1]
         self.size = self.size - 1
--- algorithmIlliminated_2/testHeap.py
+++ algorithmIlliminated_2/testHeap.py
@@ -1,6 +1,6 @@
 import unittest
 import random
-from algorithmIlliminated_2.heap import Heap
+from algorithmIlliminated_2.heap import *
 from utils.utils import *
 
 
@@ -12,7 +12,7 @@ class MyTestCase(unittest.TestCase):
         return items
 
     def test_buildMinHeap(self):
-        heap = Heap()
+        heap = Heap(HeapType.MIN)
         keys = [5, 4, 1, 2, 6, 3, 8, 7, 9, 0]
         heap.buildHeap(self.buildItems(keys))
         heap.insert(13, None)
@@ -23,7 +23,19 @@ class MyTestCase(unittest.TestCase):
             item = heap.extract_min()
             self.assertTrue(i == item[0])
 
-    def test_shuffle_heap_ops(self):
+    def test_buildMaxHeap(self):
+        heap = Heap(HeapType.MAX)
+        keys = [5, 4, 1, 2, 6, 3, 8, 7, 9, 0]
+        heap.buildHeap(self.buildItems(keys))
+        heap.insert(13, None)
+        heap.insert(11, None)
+        heap.insert(12, None)
+        heap.insert(10, None)
+        for i in range(13, -1, -1):
+            item = heap.extract_max()
+            self.assertTrue(i == item[0])
+
+    def test_shuffle_max_heap_ops(self):
         startMillSnds = current_milli_time()
         keys = []
         cnt = 1024 * 10
@@ -31,22 +43,21 @@ class MyTestCase(unittest.TestCase):
             keys.append(i)
         random.shuffle(keys)
 
-        heap = Heap()
+        heap = Heap(HeapType.MAX)
         heap.buildHeap(self.buildItems(keys))
-        item = heap.extract_min()
-        self.assertTrue(4 == item[0])
+        for i in range(cnt - 1, 3, -1):
+            item = heap.extract_max()
+            self.assertTrue(i == item[0])
+
         heap.insert(2, None)
-        item = heap.extract_min()
+        item = heap.extract_max()
         self.assertTrue(2 == item[0])
         heap.insert(1, None)
         heap.insert(3, None)
-        item = heap.extract_min()
-        self.assertTrue(1 == item[0])
-        item = heap.extract_min()
+        item = heap.extract_max()
         self.assertTrue(3 == item[0])
-        for i in range(5, cnt, 1):
-            item = heap.extract_min()
-            self.assertTrue(i == item[0])
+        item = heap.extract_max()
+        self.assertTrue(1 == item[0])
         endMilliSnds = current_milli_time()
         print("time used to process 10K heaps [%d] ms" % (endMilliSnds - startMillSnds))
 
