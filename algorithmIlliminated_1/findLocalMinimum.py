9de7daadcb98a7f3e41c4f97797e1e3d7f1c271d
Yongjun Chai <yongjunchai@hotmail.com>
2020-02-15 20:31:15 -0800

    add unit test for find local minimum

--- algorithmIlliminated_1/findLocalMinimum.py
+++ algorithmIlliminated_1/findLocalMinimum.py
@@ -5,6 +5,25 @@ class FindLocalMinimum:
     def __init__(self):
         pass
 
+    @staticmethod
+    def isLocalMinimum(matrix, rows: int, cols: int, startRow: int, startColumn: int, rowOffset: int, colOffset: int):
+        minRow = startRow + rowOffset
+        minCol = startColumn + colOffset
+        value = matrix[minRow][minCol]
+        if minRow - 1 >= startRow:
+            if value > matrix[minRow - 1][minCol]:
+                return False
+        if minRow + 1 < startRow + rows:
+            if value > matrix[minRow + 1][minCol]:
+                return False
+        if minCol - 1 >= startColumn:
+            if value > matrix[minRow][minCol - 1]:
+                return False
+        if minCol + 1 < startColumn + cols:
+            if value > matrix[minRow][minCol + 1]:
+                return False
+        return True
+
     def findLocalMinimum(self, matrix, rows: int, columns: int):
         """
         solve the issue through divide-and-conquer
@@ -16,28 +35,67 @@ class FindLocalMinimum:
         return self.findLocalMinimum_internal(matrix, rows, columns, 0, 0)
 
     def findLocalMinimum_internal(self, matrix, rows: int, columns: int, startRow: int, startColumn: int):
-        assert(rows == columns)
-        assert(startRow == startColumn)
-        assert(rows >= 2)
-        if rows == 2:
-            i00 = matrix[startRow][startColumn]
-            i01 = matrix[startRow][startColumn + 1]
-            i10 = matrix[startRow + 1][startColumn]
-            i11 = matrix[startRow + 1][startColumn + 1]
-            if i00 < i01 and i00 < i10:
-                return startRow, startColumn
-            if i01 < i00 and i01 < i11:
-                return startRow, startColumn + 1
-            if i10 < i00 and i10 < i11:
-                return startRow + 1, startColumn
-            return startRow + 1, startColumn + 1
+        """
+
+        :param matrix:
+        :param rows: rows inside the small rectangle
+        :param columns: columns inside the small rectangle
+        :param startRow: bottom left of the small rectangle inside the matrix
+        :param startColumn: bottom left of the small rectangle inside the matrix
+        :return: row, column of local minimum
+        """
+        if rows < 4 or columns < 4:
+            for i in range(rows):
+                for j in range(columns):
+                    if FindLocalMinimum.isLocalMinimum(matrix, rows, columns, startRow, startColumn, i, j):
+                        return startRow + i, startColumn + j
+            return None, None
         midRow = startRow + int(rows / 2)
         midCol = startColumn + int(columns / 2)
         midPlusRow = midRow + 1
         midPlusCol = midCol + 1
-        
+        midRowMinimum_row, midRowMinimum_column = self.findMinimum_of_row(matrix, columns, midRow, startColumn)
+        midColMinimum_row, midColMinimum_column = self.findMinimum_of_column(matrix, rows, startRow, midCol)
+        midPlusRowMinimum_row, midPlusRowMinimum_column = self.findMinimum_of_row(matrix, columns, midPlusRow, startColumn)
+        midPlusColMinimum_row, midPlusColMinimum_column = self.findMinimum_of_column(matrix, rows, startRow, midPlusCol)
+        points = [(midRowMinimum_row, midRowMinimum_column),
+                  (midColMinimum_row, midColMinimum_column),
+                  (midPlusRowMinimum_row, midPlusRowMinimum_column),
+                  (midPlusColMinimum_row, midPlusColMinimum_column)
+                  ]
+        minRow, minCol = self.findMinimum(matrix, points)
+        if minRow <= midRow and minCol <= midCol:
+            # bottom left quadrant
+            return self.findLocalMinimum_internal(matrix, midRow - startRow + 1, midCol - startColumn + 1, startRow, startColumn)
+        elif minRow <= midRow and minCol >= midPlusCol:
+            # bottom right quadrant
+            return self.findLocalMinimum_internal(matrix, midRow - startRow + 1, columns - (midPlusCol - startColumn), startRow, midPlusCol)
+        elif minRow >= midPlusRow and minCol <= midCol:
+            # top left quadrant
+            return self.findLocalMinimum_internal(matrix, rows - (midPlusRow - startRow), midCol - startColumn + 1, midPlusRow, startColumn)
+        else:
+            # top right quadrant
+            return self.findLocalMinimum_internal(matrix, rows - (midPlusRow - startRow), columns - (midPlusCol - startColumn), midPlusRow, midPlusCol)
 
+    def findMinimum(self, maxtrix, points: list):
+        minRow, minCol = points[0][0], points[0][1]
+        for i in range(1, len(points)):
+            if maxtrix[minRow][minCol] > maxtrix[points[i][0]][points[i][1]]:
+                minRow = points[i][0]
+                minCol = points[i][1]
+        return minRow, minCol
 
+    def findMinimum_of_column(self, matrix, rows: int, startRow: int, startColumn: int):
+        minRow, minCol = startRow, startColumn
+        for i in range(1, rows):
+            if matrix[minRow][minCol] > matrix[startRow + i][startColumn]:
+                minRow = startRow + i
+        return minRow, minCol
 
-
+    def findMinimum_of_row(self, matrix, columns: int, startRow: int, startColumn: int):
+        minRow, minCol = startRow, startColumn
+        for i in range(1, columns):
+            if matrix[minRow][minCol] > matrix[startRow][startColumn + i]:
+                minCol = startColumn + i
+        return minRow, minCol
 
---
+++ algorithmIlliminated_1/testFindLocalMinimum.py
@@ -0,0 +1,59 @@
+import unittest
+import random
+from algorithmIlliminated_1.findLocalMinimum import *
+from utils.utils import *
+
+
+class MyTestCase(unittest.TestCase):
+    def createOrderedData(self, rows: int, columns: int):
+        assert(rows == columns)
+        assert(rows >= 2)
+        nums = Utils.createArray([rows, columns])
+        for i in range(rows):
+            for j in range(columns):
+                nums[i][j] = i * columns + j
+        return nums
+
+    def createRandomData(self, rows: int, columns: int):
+        nums = []
+        for i in range(rows * columns):
+            nums.append(i)
+        random.shuffle(nums)
+        rndNums = Utils.createArray([rows, columns])
+        for i in range(rows):
+            for j in range(columns):
+                rndNums[i][j] = nums[i * columns + j]
+        return rndNums
+
+    def test_something(self):
+        for j in range(2, 1000):
+            i = 9
+            print(j)
+            timerData = Timer()
+            timerData.start()
+            #orderedData = self.createOrderedData(i, i)
+            # Utils.dumpMatrix(orderedData, i, i)
+            timerData.stop()
+            #print("creation data time %d ms" % timerData.timeElapsedMs())
+            rndData = self.createRandomData(i, i)
+            timer = Timer()
+            timer.start()
+            findLocalMinimum = FindLocalMinimum()
+            #row, col = findLocalMinimum.findLocalMinimum(orderedData, i, i)
+            #self.assertTrue(FindLocalMinimum.isLocalMinimum(orderedData, i, i, 0, 0, row, col))
+            timer.stop()
+            #print("time elapsed %d ms" % timer.timeElapsedMs())
+            Utils.dumpMatrix(rndData, i, i)
+            row, col = findLocalMinimum.findLocalMinimum(rndData, i, i)
+            isLocalMin = FindLocalMinimum.isLocalMinimum(rndData, i, i, 0, 0, row, col)
+            #if not isLocalMin:
+            #    print("fail")
+            if not isLocalMin:
+                print("(%d, %d)" % (row, col))
+                row, col = findLocalMinimum.findLocalMinimum(rndData, i, i)
+                isLocalMin = FindLocalMinimum.isLocalMinimum(rndData, i, i, 0, 0, row, col)
+
+            self.assertTrue(isLocalMin)
+
+if __name__ == '__main__':
+    unittest.main()
